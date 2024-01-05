c3f4ab15ac38b0073983a9158a317e635a489490
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-05 17:35:57 -0300

    [ :sparkles: ] | create: script - close loading screen (app/script)

---
+++ src/script.js
@@ -0,0 +1,14 @@
+"use strict";
+
+/**
+ * LOADING
+ *
+ * close loading screen after page load
+ */
+
+const loading = document.querySelector("[data-loading]");
+
+window.addEventListener("load", () => {
+  loading.classList.add("loaded");
+  document.body.classList.add("loaded");
+});
