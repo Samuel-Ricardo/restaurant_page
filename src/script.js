a2920ddd89e23a882a1b9563d9e7b1f1f29e5431
samuel_ricardo <samuelricardoofficial@gmail.com>
2024-01-06 16:06:34 -0300

    [ :sparkles: ] | create: util - addEventOnElements (app)

--- src/script.js
+++ src/script.js
@@ -1,6 +1,18 @@
 "use strict";
 
 /**
+ *
+ * UTILS
+ *
+ */
+
+const addEventOnElements = function (elements, eventType, callback) {
+  for (let i = 0, len = elements.length; i < len; i++) {
+    elements[i].addEventListener(eventType, callback);
+  }
+};
+
+/**
  * LOADING
  *
  * close loading screen after page load
@@ -12,3 +24,15 @@ window.addEventListener("load", () => {
   loading.classList.add("loaded");
   document.body.classList.add("loaded");
 });
+
+const navbar = document.querySelector("[daata-navbar]");
+const navTogglers = document.querySelector("[data-nav-toggler]");
+const overlay = document.querySelector("[data-overlay]]");
+
+const toggleNavbar = () => {
+  navbar.classList.toggle("active");
+  overlay.classList.toggle("active");
+  document.body.classList.toggle("nav-active");
+};
+
+addEventOn;
